7ed59adfc948fa0b1b817c710d84a49a0eca2cee
trrez <tatianagutierrez15@gmail.com>
2023-06-13 16:54:34 -0400

    done

--- fundamentals/fundamentals/cuentaBancaria.py
+++ fundamentals/fundamentals/cuentaBancaria.py
@@ -36,9 +36,38 @@ class CuentaBancaria:
             cuenta.mostrar_info_cuenta()
 
 
-usr1 = CuentaBancaria()
-usr2 = CuentaBancaria()
-usr1.deposito(200).deposito(200).deposito(100).retiro(200).generar_interes()
-usr2.deposito(600).deposito(500).retiro(100).retiro(
-    200).retiro(200).retiro(100).generar_interes()
-CuentaBancaria.imprimir_todas_las_cuentas()
+class Usuario:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.cuenta = CuentaBancaria(tasa_interes=0.02, balance=0)
+
+    def hacer_deposito(self, amount):
+        self.cuenta.balance += amount
+        return self
+
+    def hacer_retiro(self, amount):
+        self.cuenta.balance -= amount
+        return self
+
+    def mostrar_balance(self):
+        print(
+            f"Nombre: {self.name} Email: {self.email} Balance: {self.cuenta.balance}")
+        return self
+
+
+usr1 = Usuario("Tatiana", "tatiana@gmail.com")
+usr2 = Usuario("Ignacia", "Ignacia@gmail.com")
+usr2.hacer_deposito(300).hacer_deposito(300).hacer_retiro(500)
+usr1.hacer_deposito(200).hacer_deposito(
+    200).hacer_deposito(200).hacer_retiro(400)
+usr1.mostrar_balance()
+usr1.cuenta.mostrar_info_cuenta()
+
+
+# usr1 = CuentaBancaria()
+# usr2 = CuentaBancaria()
+# usr1.deposito(200).deposito(200).deposito(100).retiro(200).generar_interes()
+# usr2.deposito(600).deposito(500).retiro(100).retiro(
+#     200).retiro(200).retiro(100).generar_interes()
+# CuentaBancaria.imprimir_todas_las_cuentas()
